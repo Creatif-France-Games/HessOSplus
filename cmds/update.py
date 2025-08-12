0e893e6506a8429f6c25964dfef19a0fa49b3525
Bowser <164537694+bowser-2077@users.noreply.github.com>
2025-08-12 13:21:36 +0200

    Update update.py

--- cmds/update.py
+++ cmds/update.py
@@ -4,11 +4,16 @@ import io
 import os
 import shutil
 import sys
+import time
 
 def run(args):
     zip_url = "https://github.com/bowser-2077/HessOS/archive/refs/heads/main.zip"
 
     print("Downloading latest HessOS update...")
+    print("Make sure to relaunch the OS if you experiencing bug after the update.")
+    print("You can also try 'reload-cmds' but its really recommended to restart!")
+    print("Waiting 10 seconds.")
+    time.sleep(10)
     try:
         r = requests.get(zip_url)
         r.raise_for_status()
@@ -40,6 +45,8 @@ def run(args):
     shutil.rmtree(tmp_dir)
 
     print("Update complete! Restarting HessOS now...")
+    time.sleep(3)
 
     python = sys.executable
     os.execv(python, [python] + sys.argv)
+
